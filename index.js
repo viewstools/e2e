9d37455eff7f77a215887123747f9fbfce80ac5e
amy mccarthy <amy.mccarthy2@mail.dcu.ie>
2018-01-31 16:39:07 +0000

    feat: refactor to get lists (#3)
    * feat: refator to get lists
    
    * chore: add comments
    
    * refactor: fix scope problem
    
    * fix: pass all!

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const key = process.env.VIEWS_E2E_TEST_ID || "data-test-id";
+const key = process.env.VIEWS_E2E_TEST_ID || 'data-test-id';
 
-const get = (id, ...scopes) => () => {
-  const el = browser.element(`[${key}*="${id}|"]`);
+const getElBySelector = ($in, selector, index, all) => {
+  let el;
+  if (typeof index === 'number') {
+    // get item from list by index
+    el = $in.elements(selector).value[index];
+  } else if (all) {
+    // get all items from list
+    el = $in.elements(selector).value;
+  } else {
+    el = $in.element(selector);
+  }
+  return el;
+};
+
+const get = (id, ...scopes) => ({ all = false, $in = browser, index } = {}) => {
+  const idSelector = `[${key}*="${id}|"]`;
+
+  let el = getElBySelector($in, idSelector, index, all);
 
   scopes.forEach(scope => {
     el[scope] = () =>
-      browser.element(`[${key}*="${id}|"][${key}*="${scope}|"]`);
+      getElBySelector($in, `${idSelector}[${key}*="${scope}|"`, index, all);
   });
 
   return el;
 };
 
 module.exports = {
-  get
+  get,
 };
