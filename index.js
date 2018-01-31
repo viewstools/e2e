7ced6e3c6feba09e4a361c2a34d38346e71b947c
amymc <amy.mccarthy@hmhco.com>
2018-01-31 16:23:03 +0000

    chore: add comments

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const get = (id, ...scopes) => ({ all = false, $in = browser, index } = {}) => {
 
   const getElBySelector = selector => {
     if (typeof index === 'number') {
+      // get item from list by index
       el = $in.elements(selector).value[index];
     } else if (all) {
+      // get all items from list
       el = $in.elements(selector).value;
     } else {
       el = $in.element(selector);
