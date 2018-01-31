33ce57e855b162c997d2eaefde67ff0f6e48a997
amymc <amy.mccarthy@hmhco.com>
2018-01-31 16:21:08 +0000

    feat: refator to get lists

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const key = process.env.VIEWS_E2E_TEST_ID || "data-test-id";
+const key = process.env.VIEWS_E2E_TEST_ID || 'data-test-id';
 
-const get = (id, ...scopes) => () => {
-  const el = browser.element(`[${key}*="${id}|"]`);
+const get = (id, ...scopes) => ({ all = false, $in = browser, index } = {}) => {
+  const idSelector = `[${key}*="${id}|"]`;
+
+  const getElBySelector = selector => {
+    if (typeof index === 'number') {
+      el = $in.elements(selector).value[index];
+    } else if (all) {
+      el = $in.elements(selector).value;
+    } else {
+      el = $in.element(selector);
+    }
+  };
+
+  getElBySelector(idSelector);
 
   scopes.forEach(scope => {
-    el[scope] = () =>
-      browser.element(`[${key}*="${id}|"][${key}*="${scope}|"]`);
+    el[scope] = () => {
+      getElBySelector(`${idSelector}[${key}*="${scope}|"`);
+    };
   });
 
   return el;
 };
 
 module.exports = {
-  get
+  get,
 };
