e844de63d44fc35a5233306ffbff98e3474fe7f4
amymc <amy.mccarthy@hmhco.com>
2018-01-31 16:34:42 +0000

    refactor: fix scope problem

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const key = process.env.VIEWS_E2E_TEST_ID || 'data-test-id';
 
+const getElBySelector = ($in, selector, index, all) => {
+  let el;
+  if (typeof index === 'number') {
+    // get item from list by index
+    el = $in.elements(selector).value[index];
+  } else if (all) {
+    // get all items from list
+    el = $in.elements(selector).value;
+  } else {
+    el = $in.element(selector);
+  }
+  return el;
+};
+
 const get = (id, ...scopes) => ({ all = false, $in = browser, index } = {}) => {
   const idSelector = `[${key}*="${id}|"]`;
 
-  const getElBySelector = selector => {
-    if (typeof index === 'number') {
-      // get item from list by index
-      el = $in.elements(selector).value[index];
-    } else if (all) {
-      // get all items from list
-      el = $in.elements(selector).value;
-    } else {
-      el = $in.element(selector);
-    }
-  };
-
-  getElBySelector(idSelector);
+  let el = getElBySelector($in, idSelector, index, all);
 
   scopes.forEach(scope => {
-    el[scope] = () => {
-      getElBySelector(`${idSelector}[${key}*="${scope}|"`);
-    };
+    el[scope] = () =>
+      getElBySelector($in, `${idSelector}[${key}*="${scope}|"`, index);
   });
 
   return el;
